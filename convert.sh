1342f45c9f9f0a435b829faa4aab6c829e93907b
codl (Corentin Delcourt) <codl@codl.fr>
2015-07-01 08:17:36 +0200

    lower bitrates

--- convert.sh
+++ convert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ls *.flac | while read -r f; do
-    ffmpeg -nostdin -y -i "$f" -c:a libopus -b:a 200k "${f%.flac}.opus" || exit $?
-    ffmpeg -nostdin -y -i "$f" -c:a libmp3lame -q 1 "${f%.flac}.mp3" || exit $?
+    ffmpeg -nostdin -y -i "$f" -c:a libopus -b:a 160k "${f%.flac}.opus" || exit $?
+    ffmpeg -nostdin -y -i "$f" -c:a libmp3lame -q 3 "${f%.flac}.mp3" || exit $?
 done
