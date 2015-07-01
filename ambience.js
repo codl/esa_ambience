4d1ba0874ecd5bdc5d71459c21a899a49daab5b8
codl (Corentin Delcourt) <codl@codl.fr>
2015-07-01 08:50:09 +0200

    add fading in/out

--- ambience.js
+++ ambience.js
@@ -1,26 +1,48 @@
 window.ambience = function() {
     var inputs = document.querySelectorAll(".chooser input");
     var audios = {};
+
     for (var i = 0; i < inputs.length; i++){
         var input = inputs[i];
         var filename_prefix = input.dataset.filenamePrefix;
 
         var audio = document.createElement("audio");
         audio.loop = true;
+        audio.volume = 0;
 
         (function(audio, input){
+            var fade;
+            var fadeinout = function fadeinout(sign){
+                if(fade){
+                    clearTimeout(fade);
+                    fade = null;
+                }
 
+                var newvol = audio.volume + .04 * sign;
+                audio.volume = Math.max(0, Math.min(1, newvol));
 
+                if(sign >= 0 && audio.volume >= 0.99){
+                    return
+                }
+                if(sign < 0 && audio.volume <= 0.01){
+                    audio.pause();
+                    return;
+                }
 
+                var fade = setTimeout(fadeinout, 500, sign);
+            }
+            audio.fadein = function fadein(){ fadeinout(+1); };
+            audio.fadeout = function fadeout(){ fadeinout(-1); };
 
             var change = function change(){
                 if(input.checked){
                     for(var id in audios){
                         if(id == input.id){
                             audios[id].play();
+                            audios[id].fadein();
                         }
                         else if(!audios[id].paused) {
-                            audios[id].pause();
+                            audios[id].fadeout();
                         }
                     }
                 }
